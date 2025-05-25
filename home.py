eee440106224e8678123a3641e21128b57fe784b
Ibnu Pujiono <ibnu.treasury@gmail.com>
2025-05-26 07:34:51 +1000

    update 260525_8

--- home.py
+++ home.py
@@ -655,7 +655,7 @@ if menu == "Mapping Strata":
     )
 
 if menu == "Chatbot":
-    llm = ChatGroq(temperature=0, model="llama-3.2-90b-text-preview", api_key=groq_api_key)
+    llm = ChatGroq(temperature=0, model="llama3-70b-8192", api_key=groq_api_key)
     #llm = ChatOpenAI(model="gpt-4-turbo", temperature=0,api_key=openai_api_key)
     agent = create_csv_agent(llm, "./completed_data_stratxcel.csv", verbose=True, allow_dangerous_code=True, max_execution_time=1000000000000000)
     def query_data(query):
